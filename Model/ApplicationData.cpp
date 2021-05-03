218a779709d6c88d3daf3581cfbdeb4334fd30af
yaitichou <62092385+yaitichou@users.noreply.github.com>
2021-05-03 17:30:39 +0200

    Update ApplicationData.cpp

--- Model/ApplicationData.cpp
+++ Model/ApplicationData.cpp
@@ -1,33 +1,33 @@
 #include "ApplicationData.h"
 
-List Model::ApplicationData::getCleanerList() {
+List ApplicationData::getCleanerList() {
 	return this->cleanerList;
 }
 
-void Model::ApplicationData::addCleaner(Model::Cleaner cleaner) {
+void ApplicationData::addCleaner(Model::Cleaner cleaner) {
 	// TODO - implement ApplicationData::addCleaner
 	throw "Not yet implemented";
 }
 
-List Model::ApplicationData::getSensorList() {
+List ApplicationData::getSensorList() {
 	return this->sensorList;
 }
 
-void Model::ApplicationData::addSensor(Model::Sensor sensor) {
+void ApplicationData::addSensor(Model::Sensor sensor) {
 	// TODO - implement ApplicationData::addSensor
 	throw "Not yet implemented";
 }
 
-List Model::ApplicationData::getUserList() {
+List ApplicationData::getUserList() {
 	return this->userList;
 }
 
-void Model::ApplicationData::addUser(Model::User user) {
+void ApplicationData::addUser(Model::User user) {
 	// TODO - implement ApplicationData::addUser
 	throw "Not yet implemented";
 }
 
-float Model::ApplicationData::distance(float lat1, float lon1, float lat2, float lon2) {
+float ApplicationData::distance(float lat1, float lon1, float lat2, float lon2) {
 	// TODO - implement ApplicationData::distance
 	throw "Not yet implemented";
 }
