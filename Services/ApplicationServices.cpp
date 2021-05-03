b0c96a40eab482f24668a1ffc44d3a2fa01f4317
yaitichou <62092385+yaitichou@users.noreply.github.com>
2021-05-03 17:43:14 +0200

    Update ApplicationServices.cpp

--- Services/ApplicationServices.cpp
+++ Services/ApplicationServices.cpp
@@ -1,36 +1,36 @@
 #include "ApplicationServices.h"
 
-Model::ReliabilityFlag Services::ApplicationServices::checkSensorsReliabilities(Date uTBegin, Date uTEnd) {
+ReliabilityFlag ApplicationServices::checkSensorsReliabilities(Date uTBegin, Date uTEnd) {
 	// TODO - implement ApplicationServices::checkSensorsReliabilities
 	throw "Not yet implemented";
 }
 
-void Services::ApplicationServices::flagSensor(int uSensorID, bool uFlag) {
+void ApplicationServices::flagSensor(int uSensorID, bool uFlag) {
 	// TODO - implement ApplicationServices::flagSensor
 	throw "Not yet implemented";
 }
 
-List Services::ApplicationServices::compareSensorSimilarities(int uSensorID, Date uTBegin, Date uTEnd) {
+list ApplicationServices::compareSensorSimilarities(int uSensorID, Date uTBegin, Date uTEnd) {
 	// TODO - implement ApplicationServices::compareSensorSimilarities
 	throw "Not yet implemented";
 }
 
-float Services::ApplicationServices::getAreaAirQuality(float uLat, float uLon, float uRad, Date uTBegin, Date uTEnd) {
+float ApplicationServices::getAreaAirQuality(float uLat, float uLon, float uRad, Date uTBegin, Date uTEnd) {
 	// TODO - implement ApplicationServices::getAreaAirQuality
 	throw "Not yet implemented";
 }
 
-float Services::ApplicationServices::getPunctualAirQuality(float uLat, float uLon, Date uTBegin, Date uTEnd) {
+float ApplicationServices::getPunctualAirQuality(float uLat, float uLon, Date uTBegin, Date uTEnd) {
 	// TODO - implement ApplicationServices::getPunctualAirQuality
 	throw "Not yet implemented";
 }
 
-Array Services::ApplicationServices::getCleanerContribution(int uCleanerID) {
+Array ApplicationServices::getCleanerContribution(int uCleanerID) {
 	// TODO - implement ApplicationServices::getCleanerContribution
 	throw "Not yet implemented";
 }
 
-void Services::ApplicationServices::importData() {
+void ApplicationServices::importData() {
 	// TODO - implement ApplicationServices::importData
 	throw "Not yet implemented";
 }
