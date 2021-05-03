10d6264ec695ff6d677a609efac23d30fcc30c75
yaitichou <62092385+yaitichou@users.noreply.github.com>
2021-05-03 17:44:25 +0200

    Update UserManagement.cpp

--- Services/UserManagement.cpp
+++ Services/UserManagement.cpp
@@ -1,24 +1,24 @@
 #include "UserManagement.h"
 
-Model::User Services::UserManagement::getCurrentUser() {
+User UserManagement::getCurrentUser() {
 	return this->currentUser;
 }
 
-void Services::UserManagement::setCurrentUser(Model::User currentUser) {
+void UserManagement::setCurrentUser(User currentUser) {
 	this->currentUser = currentUser;
 }
 
-Model::User Services::UserManagement::authenticate(String uEmail, String uPassword) {
+User UserManagement::authenticate(string uEmail, string uPassword) {
 	// TODO - implement UserManagement::authenticate
 	throw "Not yet implemented";
 }
 
-bool Services::UserManagement::registerCompany(int companyId) {
+bool UserManagement::registerCompany(int companyId) {
 	// TODO - implement UserManagement::registerCompany
 	throw "Not yet implemented";
 }
 
-bool Services::UserManagement::registerIndividual(String uEmail, String uPassword) {
+bool UserManagement::registerIndividual(string uEmail, string uPassword) {
 	// TODO - implement UserManagement::registerIndividual
 	throw "Not yet implemented";
 }
