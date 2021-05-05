c8a9c0aa7743c279f7314991d5c3c38d41795055
Martin Thibaud <martin.thibaud@dbmail.com>
2021-05-06 00:12:56 +0200

    Add prototypes of DisplayManager

--- Display/DisplayManager.cpp
+++ Display/DisplayManager.cpp
@@ -1,1 +1,61 @@
 #include "DisplayManager.h"
+
+// Menu displays
+void DisplayManager::displayLoginMenu(){
+
+}
+
+void DisplayManager::displayMainMenu(){
+
+}
+
+void DisplayManager::displaySensorMenu(){
+
+}
+
+// Sensor queries
+void DisplayManager::querySensorReliability(){
+
+}
+
+void DisplayManager::querySensorFlag(){
+
+}
+
+void DisplayManager::querySensorSimilarity(){
+
+}
+
+void DisplayManager::queryAreaAirQuality(){
+
+}
+
+void DisplayManager::queryPunctualAirQuality(){
+
+}
+
+// Cleaner queries
+void DisplayManager::queryCleanerContribution(){
+
+}
+
+// User queries
+void DisplayManager::queryIndividualPoints(){
+
+}
+
+void DisplayManager::queryLogin(){
+
+}
+
+void DisplayManager::queryLogout(){
+
+}
+
+void DisplayManager::queryIndividualRegister(){
+
+}
+
+void DisplayManager::queryCompanyRegister(){
+
+}
--- Display/DisplayManager.h
+++ Display/DisplayManager.h
@@ -6,7 +6,28 @@
 using namespace std;
 
 class DisplayManager {
+    public:
+        // Menu displays
+        static void displayLoginMenu();
+        static void displayMainMenu();
+        static void displaySensorMenu();
 
+        // Sensor queries
+        static void querySensorReliability();
+        static void querySensorFlag();
+        static void querySensorSimilarity();
+        static void queryAreaAirQuality();
+        static void queryPunctualAirQuality();
+
+        // Cleaner queries
+        static void queryCleanerContribution();
+
+        // User queries
+        static void queryIndividualPoints();
+        static void queryLogin();
+        static void queryLogout();
+        static void queryIndividualRegister();
+        static void queryCompanyRegister();
 };
 
 #endif
