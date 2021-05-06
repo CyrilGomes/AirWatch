f1962857a7e7c6b1cbeef9cc4b2eb52bacc9435d
dt-mark <david.toma@insa-lyon.fr>
2021-05-06 09:19:56 +0200

    atmo (NOT WORKINGGGGGG)

--- Model/Reading.cpp
+++ Model/Reading.cpp
@@ -1,5 +1,12 @@
 #include "Reading.h"
 
+unordered_map<string, int[]> atmoTable = {
+	make_pair("O3", {0, 30, 55, 80, 105, 130, 150, 180, 210, 240}),
+	make_pair("SO2", {0, 40, 80, 120, 160, 200, 250, 300, 400, 500}),
+	make_pair("NO2", {0, 30, 55, 85, 110, 135, 165, 200, 275, 400}),
+	make_pair("PM10", {0, 7, 14, 21, 28, 35, 42, 50, 65, 80})
+};
+
 Reading::Reading(Date timeStamp) : timeStamp(timeStamp) {
 }
 
@@ -26,8 +33,18 @@ void Reading::setTimeStamp(Date timeStamp) {
 }
 
 int Reading::getAtmoScore() {
-	// TODO - implement Reading::getAtmoScore
-	throw "Not yet implemented";
+	int atmo = 0;
+	for (pair<string, Measurement*> i : measurements) {
+		int subAtmo = 0;
+		for (int j = 1; j < 10; j++) {
+			if (Reading::atmoTable[i.first][j] > i.second->getValue()) {
+				subAtmo = j - 1;
+				break;
+			}
+		}
+		atmo = (subAtmo > atmo) ? subAtmo : atmo;
+	}
+	return atmo;
 }
 
 unordered_map<string, Measurement*> Reading::getMeasurements() {
