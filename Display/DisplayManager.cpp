da7d483344d222d46112d8b5e328368e98c2ce7c
Martin Thibaud <martin.thibaud@dbmail.com>
2021-05-06 15:55:59 +0200

    Add sensor menu, type def for menu option structure

--- Display/DisplayManager.cpp
+++ Display/DisplayManager.cpp
@@ -6,58 +6,68 @@
 #include <iostream>
 using namespace std;
 
+typedef pair<string, function<void()>> Option;
+
 // Menu displays
-int DisplayManager::promptMenuChoice(string prompt, vector<pair<string, function<void()>>> menuItems) {
+int DisplayManager::promptMenuChoice(string prompt, vector<Option> menuItems) {
 
     cout << prompt << " :" << endl;
     int count = 1;
-    for (pair<string, function<void()>> item : menuItems)
+    for (Option item : menuItems)
     {
         cout << count << ". " << item.first << endl;
         count++;
     }
     return Console::promptInteger(">");
-
 }
 
 void DisplayManager::displayLoginMenu()
 {
-    vector<pair<string, function<void()>>> optionsList = {
-        pair<string, function<void()>>("Login", bind(&DisplayManager::queryLogin, this)),
-        pair<string, function<void()>>("Register", bind(&DisplayManager::queryIndividualRegister, this))
+    vector<Option> optionsList = {
+        Option("Login", bind(&DisplayManager::queryLogin, this)),
+        Option("Register", bind(&DisplayManager::queryIndividualRegister, this))
     };
 
-    optionsList[promptMenuChoice("WELCOME TO AIRWATCH:", optionsList) - 1].second();
+    optionsList[promptMenuChoice("WELCOME TO AIRWATCH", optionsList) - 1].second();
 }
 
 void DisplayManager::displayMainMenu()
 {
     User currentUser = UserManagement::getCurrentUser();
 
-    vector<pair<string, function<void()>>> optionsList;
+    vector<Option> optionsList;
 
-    optionsList.insert(optionsList.end(), pair<string, function<void()>>("Sensor analytics", bind(&DisplayManager::displaySensorMenu, this)));
-    optionsList.insert(optionsList.end(), pair<string, function<void()>>("Cleaner analytics", bind(&DisplayManager::queryCleanerContribution, this)));
+    optionsList.insert(optionsList.end(), Option("Sensor analytics", bind(&DisplayManager::displaySensorMenu, this)));
+    optionsList.insert(optionsList.end(), Option("Cleaner analytics", bind(&DisplayManager::queryCleanerContribution, this)));
 
     UserType userType = currentUser.getType();
     switch (userType)
     {
     case UserType::company:
-        optionsList.insert(optionsList.end(), pair<string, function<void()>>("Register Air Cleaning Company", bind(&DisplayManager::queryCompanyRegister, this)));
+        optionsList.insert(optionsList.end(), Option("Register Air Cleaning Company", bind(&DisplayManager::queryCompanyRegister, this)));
         break;
 
     case UserType::individual:
-        optionsList.insert(optionsList.end(), pair<string, function<void()>>("My user points", bind(&DisplayManager::queryIndividualPoints, this)));
+        optionsList.insert(optionsList.end(), Option("My user points", bind(&DisplayManager::queryIndividualPoints, this)));
         break;
     }
 
-    optionsList.insert(optionsList.end(), pair<string, function<void()>>("Log out", bind(&DisplayManager::queryLogout, this)));
+    optionsList.insert(optionsList.end(), Option("Log out", bind(&DisplayManager::queryLogout, this)));
 
     optionsList[promptMenuChoice("Main menu:", optionsList) - 1].second();
 }
 
 void DisplayManager::displaySensorMenu()
 {
+    vector<Option> optionsList = {
+        Option("Get the air quality of a given location", bind(&DisplayManager::queryPunctualAirQuality, this)),
+        Option("Get the air quality of a given area", bind(&DisplayManager::queryAreaAirQuality, this)),
+        Option("See sensor reliabilities", bind(&DisplayManager::querySensorReliability, this)),
+        Option("Find data similarities", bind(&DisplayManager::querySensorSimilarity, this)),
+        Option("Go back", bind(&DisplayManager::displayMainMenu, this))
+    };
+
+    optionsList[promptMenuChoice("Sensor analytics menu:", optionsList) - 1].second();
 }
 
 // Sensor queries
